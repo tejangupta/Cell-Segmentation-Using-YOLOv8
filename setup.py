eb939e443ea967239b77aa333f1a3a084400a427
tejangupta <tejangupta8@gmail.com>
2023-08-27 20:03:03 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='cellSegmentation',
+    version='0.0.0',
+    author='Tejan Gupta',
+    author_email='tejangupta8@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
