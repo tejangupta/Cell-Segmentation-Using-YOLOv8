2c2f17f416a064d46995e6a477d70bec405afc84
tejangupta <tejangupta8@gmail.com>
2023-09-02 02:05:53 +0530

    model trainer code to be tested

--- cellSegmentation/constant/training_pipeline/__init__.py
+++ cellSegmentation/constant/training_pipeline/__init__.py
@@ -14,3 +14,12 @@ Data Validation related constants start with DATA_VALIDATION VAR NAME
 DATA_VALIDATION_DIR_NAME: str = "data_validation"
 DATA_VALIDATION_STATUS_FILE = 'status.txt'
 DATA_VALIDATION_ALL_REQUIRED_FILES = ["train", "valid", "test", "data.yaml"]
+
+
+"""
+MODEL TRAINER related constant start with MODEL_TRAINER var name
+"""
+MODEL_TRAINER_DIR_NAME: str = "model_trainer"
+MODEL_TRAINER_PRETRAINED_WEIGHT_NAME: str = "yolov8m-seg.pt"
+MODEL_TRAINER_NO_EPOCHS: int = 50
+MODEL_TRAINER_BATCH_SIZE: int = 16
